0399584e11c36b8c5fcddee3c07d840788673cb6
zoujieorg <zoujieorg@gmail.com>
2016-09-28 00:23:55 +0800

    9月27日晚更新

--- init.el
+++ init.el
@@ -2,10 +2,17 @@
 
 (package-initialize)
 
+;;系统自带的包
+(require 'org)
+
+;;recentf相关
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+
 (add-to-list 'load-path "~/.emacs.d/lisp/")
-
-
-
 (require 'init-packages)
 (require 'init-functions)
 (require 'init-better-defaults)
--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -1,13 +1,22 @@
-
-
+;;全屏
+(setq initial-frame-alist '((fullscreen . maximized)))
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+;;关闭缩进
 (electric-indent-mode -1)
 (setq inhibit-splash-screen t)
 (global-linum-mode t)
 (setq-default cursor-type 'bar)
 (global-company-mode t)
+;;取消"~"结尾的自动备份文件。
+(setq make-backup-files nil)
+;;选择文本内容时，如有键入，则选择内容被替换
+(delete-selection-mode t)
 
+;;注释文件中的语法高亮
+(setq org-src-fontify-natively t)
 
+;;自动添加()成对
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
 (provide 'init-better-defaults)
